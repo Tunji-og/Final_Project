4992e900dc085793d21c4deb53ee91aac6ef885b
adetunjiogunsusi@gmail.com <adetunjiogunsusi@gmail.com>
2023-04-03 12:47:24 +0100

    Imported the datasets manually

--- app.py
+++ app.py
@@ -146,13 +146,14 @@ def predict_diagnosis(data):
 def page_predictions():
     import streamlit as st
     import pandas as pd
-    from sklearn.datasets import load_breast_cancer
+    #rom sklearn.datasets import load_breast_cancer
     from sklearn.model_selection import train_test_split
     from sklearn.preprocessing import StandardScaler
     from sklearn.svm import SVC
 
     # Load the Wisconsin Breast Cancer dataset
-    data = load_breast_cancer()
+    data = pd.read_csv('breast_cancer.csv')
+
 
     # Split the dataset into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.2, random_state=42)
