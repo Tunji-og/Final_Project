4eb1488bb5c74c59f433c87a8aa47bcdce1a10f7
adetunjiogunsusi@gmail.com <adetunjiogunsusi@gmail.com>
2023-04-12 22:50:31 +0100

    Removed Collins And Edited the data Description Page

--- app.py
+++ app.py
@@ -17,11 +17,10 @@ import plotly.express as px
 
 # Define function for page 3: Group Members
 def page_group_members():
-    st.title("Breast Cancer Diagnosis Prediction")
-    st.title("Introduction")
-    st.title("App for Group 2's Final Year Project's Data Analysis/Machine Learning Component\n\nComputer Science Department, School of Computing and Engineering Sciences, Babcock University, Ilishan-Remo, Ogun State, Nigeria.\n\nGroup 2 Members:\n\n1. Ogunsusi, Adetunji Gabriel 19/2500\n2. Okoye, Adaora Jessica 19/0686\n3. Sawo, Collins 19/1080\n\nGroup Project Supervisor: Ernest E. Onuiri, PHD\n\r")
+    st.title("PREDICTIVE MODEL FOR BREAST CANCER DIAGNOSIS USING MACHINE LEARNING TECHNIQUES")
+    st.title("Group 2's Final Year Project's Data Analysis/Machine Learning Component\n\nComputer Science Department, School of Computing and Engineering Sciences, Babcock University, Ilishan-Remo, Ogun State, Nigeria.\n\nGroup 2 Members:\n\n1. Ogunsusi, Adetunji Gabriel 19/2500\n2. Okoye, Adaora Jessica 19/0686\n\nGroup Project Supervisor: Ernest E. Onuiri, PHD\n\r")
     # Load image from URL
-    url = "https://images.pexels.com/photos/5702171/pexels-photo-5702171.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    url = "https://images.pexels.com/photos/579474/pexels-photo-579474.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     st.image(url, caption=" ")
     
 # Define function for page 1: Data Set Description
@@ -34,15 +33,59 @@ def page_data_description():
     
     # Display data set summary and description
     st.subheader("Data Set Summary")
+    st.write(df.head(4))
+
+    st.subheader("\n\n\nData Set Description")
     st.write(df.describe())
-    st.subheader("Data Set Description")
-    st.write("This data set contains information about...")
+    st.write("This dataset contains information about the Wisconsin Breast Cancer, which was collected from breast cancer patients at the University of Wisconsin Hospitals, Madison, in the USA. The dataset consists of measurements of 30 different features, including the radius, texture, perimeter, area, smoothness, compactness, concavity, concave points, symmetry, and fractal dimension of the cell nuclei present in images of the breast tissue. Each feature is represented as a real-valued number. The dataset also includes a binary target variable indicating whether the tumor is benign or malignant. There are a total of 569 instances in the dataset, with 357 instances labeled as benign and 212 instances labeled as malignant. The dataset is commonly used in machine learning research to train models for breast cancer diagnosis and prediction.")
+    feature_descriptions = {
+    'radius': 'The mean distance from the center of the nucleus to the perimeter of the cell.',
+    'texture': 'The standard deviation of gray-scale values in the image, which is a measure of the variation in the intensity of the pixels.',
+    'perimeter': 'The total length of the cell perimeter.',
+    'area': 'The total area of the cell.',
+    'smoothness': 'A measure of the local variation in radius lengths.',
+    'compactness': 'A measure of the perimeter squared divided by the area - a high value indicates that the cells are compact.',
+    'concavity': 'The severity of concave portions of the contour of the cell.',
+    'concave points': 'The number of concave portions of the contour of the cell.',
+    'symmetry': 'A measure of the symmetry of the cell.',
+    'fractal dimension': 'A measure of the complexity of the cell boundary.',
+    'radius_mean': 'The mean value of the radius feature for the 3 largest cell nuclei in the image.',
+    'texture_mean': 'The mean value of the texture feature for the 3 largest cell nuclei in the image.',
+    'perimeter_mean': 'The mean value of the perimeter feature for the 3 largest cell nuclei in the image.',
+    'area_mean': 'The mean value of the area feature for the 3 largest cell nuclei in the image.',
+    'smoothness_mean': 'The mean value of the smoothness feature for the 3 largest cell nuclei in the image.',
+    'compactness_mean': 'The mean value of the compactness feature for the 3 largest cell nuclei in the image.',
+    'concavity_mean': 'The mean value of the concavity feature for the 3 largest cell nuclei in the image.',
+    'concave points_mean': 'The mean value of the concave points feature for the 3 largest cell nuclei in the image.',
+    'symmetry_mean': 'The mean value of the symmetry feature for the 3 largest cell nuclei in the image.',
+    'fractal_dimension_mean': 'The mean value of the fractal dimension feature for the 3 largest cell nuclei in the image.',
+    'radius_se': 'The standard error of the radius mean feature calculated across all the cells in the image.',
+    'texture_se': 'The standard error of the texture mean feature calculated across all the cells in the image.',
+    'perimeter_se': 'The standard error of the perimeter mean feature calculated across all the cells in the image.',
+    'area_se': 'The standard error of the area mean feature calculated across all the cells in the image.',
+    'smoothness_se': 'The standard error of the smoothness mean feature calculated across all the cells in the image.',
+    'compactness_se': 'The standard error of the compactness mean feature calculated across all the cells in the image.',
+    'concavity_se': 'The standard error of the concavity mean feature calculated across all the cells in the image.',
+    'concave points_se': 'The standard error of the concave points mean feature calculated across all the cells in the image.',
+    'symmetry_se': 'The standard error of the symmetry mean feature calculated across all the cells in the image.',
+    'fractal_dimension_se': 'The standard error of the fractal dimension mean feature calculated across all the cells in the image.'}
+    
+    st.write("Feature Descriptions:")
+    for feature, description in feature_descriptions.items():
+        st.markdown(f"- **{feature}:** {description}")
+
+
+
+
+
+
+
+
     
     # Display data cleaning process and visualization
     st.subheader("Data Cleaning")
     st.write("Here's how we cleaned the data:")
-    # Data Cleaning
-    st.subheader("Data Cleaning")
+
     
     # Drop columns with missing values
     df = df.dropna(axis=1)
@@ -50,11 +93,11 @@ def page_data_description():
     
     # Label encode the diagnosis column
     df['diagnosis'] = df['diagnosis'].apply(lambda x: 1 if x == 'M' else 0)
-    st.write("We label encoded the diagnosis column.")
+    st.write("We label encoded the diagnosis column: 1 for malignant 0 for Benign")
     
     # Display cleaned data set
     st.subheader("Cleaned Data Set")
-    st.write(df)
+    st.write(df.head(5))
 
     st.subheader("Data Visualization")
     st.write("Here are some visualizations of the data:")
@@ -96,6 +139,8 @@ def page_data_description():
     ax4.set_xlabel("Concavity Mean")
     ax4.set_ylabel("Concave Points")
     st.pyplot(fig4)
+    
+
 
 
 # Define function for page 2: Predictions
@@ -130,6 +175,8 @@ def predict_diagnosis(data):
     svm_model = load_svm_model() # load SVM model from pickle file
     y_pred = svm_model.predict(X_new)
     return y_pred[0], feature_names_out
+
+    
 
 # def predict_diagnosis(data):
 #     X_new = pd.DataFrame(data, index=[0])
@@ -167,7 +214,7 @@ def page_predictions():
     svm_model.fit(X_train_scaled, y_train)
 
     # Define the Streamlit app
-    st.title('Wisconsin Breast Cancer Prediction App')
+    st.title('Breast Cancer Prediction App')
 
     # Define sliders for feature input
     feature_names = data.feature_names
@@ -193,11 +240,14 @@ def page_predictions():
 
 
 
+
+
+
 # Define function for page 4: Conclusions
 def page_conclusions():
     st.title("Conclusions")
     st.write("Here are our conclusions and Recommendations from Project")
-    # ...
+    st.write("In this project, we used machine learning to develop a breast cancer prediction model using the Wisconsin Breast Cancer dataset. We tested four different models, including the CART, Gaussian NB, KNN, and SVM, and found that the SVM model performed the best with an accuracy score of 98.2%. We then deployed the model using Streamlit, which creates an interactive data science application that allows users to input patient data and receive a prediction of their breast cancer risk. While the deployed model has the potential to be used as a tool for early detection and treatment of breast cancer, there are several limitations to be considered, such as those related to data quality, generalization, and the complexity of the disease. Future work could involve improving the model's performance by incorporating more diverse datasets, additional features, and exploring more advanced machine learning techniques. Overall, this project demonstrates the potential of machine learning in the field of healthcare, and highlights the importance of continued research in the area of breast cancer prediction and treatment.")
 
 
 # Define dictionary of pages and their corresponding functions
